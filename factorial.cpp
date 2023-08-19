d0411088730b91b4af71fa475c4ffe82b699be09
anfanite396 <dipamt1729@gmail.com>
2023-08-19 15:56:32 +0530

    Handle the case for calling factorial on negative numbers

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "functions.h"
+
+int factorial(int n){
+    if (n < 0){
+        std::cout << "Enter a non-negative number" << std::endl;
+        return -1;
+    }
+    if(n!=1){
+	    return(n * factorial(n-1));
+    }
+    else
+        return 1;
+}
