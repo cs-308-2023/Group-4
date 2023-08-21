d88eb1ebb88e2250c39453c3a22b3f5d6d05afa4
Prakul <b21312@students.iitmandi.ac.in>
2023-08-21 21:10:39 +0530

    step-2 completed

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "functions.h"
+#include <cstdio>
 using namespace std;
 void print_hello(){
-   cout<<"Hello World!";
+   printf("Hello World!");
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "functions.h"
+#include <cstdio>
 using namespace std;
 int main(){
     print_hello();
-    cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    printf("\n");
+    printf("The factorial of 5 is ");
+    printf("%d",factorial(5));
+    printf("\n");
     return 0;
 }
