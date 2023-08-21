ed764ece4f12f20da2158091317fb3749d5b223e
Prakul <b21312@students.iitmandi.ac.in>
2023-08-21 21:17:10 +0530

    step-3 completed

--- factorial.cpp
+++ factorial.cpp
@@ -3,11 +3,10 @@
 
 int factorial(int n){
     if (n < 0){
-        std::cout << "Enter a non-negative number" << std::endl;
         return -1;
     }
 
-    if(n != 1)
+    else if(n > 1)
 	    return(n * factorial(n-1));
     else
         return 1;
