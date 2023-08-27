a81e75daa4f219024fd098d903e99cee8f93fe2c
Prakul <b21312@students.iitmandi.ac.in>
2023-08-27 23:46:22 +0530

    Step-5 Completed

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ int main(){
     cout<<endl;
     cout<<"The factorial of 5 is ";
     cout<<factorial(5)<<endl;
+    int x=56;
+    int y=98;
+    cout<<"The GCD of "<<x<<"and "<<y<<" is: "<<gcd(x,y)<<endl;
+    cout<<"The GCD of "<<x<<"and "<<y<<" using recursive function is: "<<gcd_rec(x,y)<<endl;
+
     return 0;
 }
