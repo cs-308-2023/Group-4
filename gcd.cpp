44e37d6d1ef3a7fd986bc1739aa46c372c8ef525
Prakul <b21312@students.iitmandi.ac.in>
2023-08-27 23:15:50 +0530

    Step-2 Completed

---
+++ gcd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include "functions.h"
+int gcd(int x,int y){
+
+	int min_ele=min(x,y);
+	int gcd_x_y=1;
+	for(int i=2;i<=min_ele;i++){
+		if(x%i==0&&y%i==0){
+			gcd_x_y=i;
+		}
+	}
+
+	return gcd_x_y;
+}
